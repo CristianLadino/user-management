5df722607cc683ca5dd0905487495ae6ba79ce85
cladino <ladidue2009@gmail.com>
2025-05-18 19:50:42 -0500

    add report jacoco

--- src/test/java/com/cristian/user_management/infrastructure/controllers/exception/ControllerHandlerExceptionTest.java
+++ src/test/java/com/cristian/user_management/infrastructure/controllers/exception/ControllerHandlerExceptionTest.java
@@ -1,7 +1,37 @@
 package com.cristian.user_management.infrastructure.controllers.exception;
 
+import com.cristian.user_management.domain.exception.DomainException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ControllerHandlerExceptionTest {
 
+    private ControllerHandlerException exceptionHandler;
+
+    @BeforeEach
+    void setUp() {
+        exceptionHandler = new ControllerHandlerException();
+    }
+
+    @Test
+    void handleDomainException_shouldReturnApiErrorResponse() {
+        var errorMessage = "Username already exists";
+        var status = HttpStatus.BAD_REQUEST;
+
+        var exception = new DomainException(errorMessage, status);
+
+        var response = exceptionHandler.handleDomainException(exception);
+
+        assertNotNull(response);
+        assertEquals(status.value(), response.getStatusCode().value());
+        assertNotNull(response.getBody());
+
+        ApiError apiError = response.getBody();
+        assertEquals(errorMessage, apiError.getError());
+        assertEquals("Business error", apiError.getMessage());
+        assertEquals(status.value(), apiError.getStatus());
+    }
 }
