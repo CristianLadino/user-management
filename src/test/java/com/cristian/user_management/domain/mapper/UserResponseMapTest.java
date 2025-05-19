9a27e8c3efd013fffea7e7fa897d0e1a2aef72e4
cladino <ladidue2009@gmail.com>
2025-05-18 19:20:23 -0500

    add unit test

---
+++ src/test/java/com/cristian/user_management/domain/mapper/UserResponseMapTest.java
@@ -0,0 +1,52 @@
+package com.cristian.user_management.domain.mapper;
+
+import com.cristian.user_management.domain.models.User;
+import com.cristian.user_management.domain.usecases.response.UserResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith({MockitoExtension.class})
+class UserResponseMapTest {
+    UserResponseMap userResponseMap;
+
+    @BeforeEach
+    void setUp() {
+        userResponseMap = Mappers.getMapper(UserResponseMap.class);
+    }
+
+    @Test
+    void toUserResponse_shouldMapFieldsCorrectly() {
+        var user = User.builder()
+                .id(1L)
+                .username("cristian")
+                .password("secret")
+                .build();
+
+        UserResponse response = userResponseMap.toUserResponse(user);
+
+        assertNotNull(response);
+        assertEquals(1L, response.getId());
+        assertEquals("cristian", response.getUsername());
+    }
+
+    @Test
+    void toUserResponseList_shouldMapListCorrectly() {
+        var user1 = User.builder().id(1L).username("cristian").password("123").build();
+        var user2 = User.builder().id(2L).username("julio").password("456").build();
+        var userList = List.of(user1, user2);
+
+        List<UserResponse> responseList = userResponseMap.toUserResponseList(userList);
+
+        assertNotNull(responseList);
+        assertEquals(2, responseList.size());
+        assertEquals("cristian", responseList.get(0).getUsername());
+        assertEquals("julio", responseList.get(1).getUsername());
+    }
+}
---
+++ src/test/java/com/cristian/user_management/infrastructure/gateway/jpa/mapper/UserMapTest.java
@@ -0,0 +1,55 @@
+package com.cristian.user_management.infrastructure.gateway.jpa.mapper;
+
+import com.cristian.user_management.domain.models.User;
+import com.cristian.user_management.infrastructure.gateway.jpa.entities.UserEntity;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mapstruct.factory.Mappers;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith({MockitoExtension.class})
+class UserMapTest {
+    UserMap userMap;
+
+    @BeforeEach
+    void setUp() {
+        userMap = Mappers.getMapper(UserMap.class);
+    }
+
+    @Test
+    void toModel_shouldMapFieldsCorrectly() {
+        var entity = UserEntity.builder()
+                .id(1L)
+                .username("cristian")
+                .password("123456")
+                .build();
+
+        User model = userMap.toModel(entity);
+
+        assertNotNull(model);
+        assertEquals(entity.getId(), model.getId());
+        assertEquals(entity.getUsername(), model.getUsername());
+        assertEquals(entity.getPassword(), model.getPassword());
+    }
+
+    @Test
+    void toEntity_shouldMapFieldsCorrectly() {
+        var user = User.builder()
+                .id(2L)
+                .username("julio")
+                .password("abcdef")
+                .build();
+
+        UserEntity entity = userMap.toEntity(user);
+
+        assertNotNull(entity);
+        assertEquals(user.getId(), entity.getId());
+        assertEquals(user.getUsername(), entity.getUsername());
+        assertEquals(user.getPassword(), entity.getPassword());
+    }
+
+}
